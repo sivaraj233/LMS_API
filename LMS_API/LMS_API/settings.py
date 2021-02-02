420514dc58098a44e5eb38280a7b1b8abc98d21d
sivaraj <siva.raj@hrblock.com>
2021-02-02 19:17:01 +0530

    API Done For Admin Login

--- LMS_API/LMS_API/settings.py
+++ LMS_API/LMS_API/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Third App
+    'api',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'corsheaders',
+    'rest_auth'
+
 ]
 
 MIDDLEWARE = [
@@ -113,6 +120,17 @@ USE_L10N = True
 
 USE_TZ = True
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.IsAdminUser',
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+    ),
+}
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
